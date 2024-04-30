4e8a46026b648ea04caab3f6aeac3f7ddd7ca888
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2024-04-30 18:14:14 +0200

    Minor test updates. Next todos: series to_list, df to_dict, vectorstore init, llm batch

--- test/example_pipelines/test_anhedonia.py
+++ test/example_pipelines/test_anhedonia.py
@@ -24,7 +24,7 @@ def test_instrumented_ml_py_pipeline_runs():
     """
     dag = run_and_assert_all_op_outputs_inspected(ANHEDONIA_ML_PY, None, ANHEDONIA_ML_PNG,
                                                   [custom_monkeypatching])
-    assert len(dag) == 52
+    assert len(dag) == 36
 
 
 def test_llm_py_pipeline_runs():
@@ -43,4 +43,4 @@ def test_instrumented_llm_py_pipeline_runs():
     """
     dag = run_and_assert_all_op_outputs_inspected(ANHEDONIA_LLM_PY, None, ANHEDONIA_LLM_PNG,
                                                   [custom_monkeypatching])
-    assert len(dag) == 52
+    assert len(dag) == 37
