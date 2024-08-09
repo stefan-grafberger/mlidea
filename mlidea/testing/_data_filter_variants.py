5bbcc27ad5c610b00355ccae91c2f13e435516b7
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2024-08-09 17:08:05 +0200

    Trying to fix final failing tests

--- mlidea/testing/_data_filter_variants.py
+++ mlidea/testing/_data_filter_variants.py
@@ -14,7 +14,7 @@ from mlidea.analysis._patch_creation import get_intermediate_extraction_patch_af
 from mlidea.analysis._what_if_analysis import WhatIfAnalysis
 from mlidea.execution._patches import PipelinePatch, ModelPatch, DataFiltering
 from mlidea.execution._pipeline_executor import singleton
-from monkeypatching._provenance_propagation import wrap_filter_func
+from mlidea.monkeypatching._provenance_propagation import wrap_filter_func
 
 
 class DataFilterVariants(WhatIfAnalysis):
