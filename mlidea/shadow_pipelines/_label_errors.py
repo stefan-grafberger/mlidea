c34771a130ff5f28066c7213eacfa37ff4676475
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2024-09-06 12:08:24 +0200

    WIP: label errors

--- mlidea/shadow_pipelines/_label_errors.py
+++ mlidea/shadow_pipelines/_label_errors.py
@@ -19,8 +19,14 @@ class LabelErrors(ShadowPipeline):
     def check_rebuilding_necessary(self, extracted_plan_results: dict[str, any]) -> any:
         return False
 
-    def __init__(self):
-        self._shadow_pipeline_id = None
+    def __init__(self, train_fraction_to_consider=1., test_fraction_to_consider=1., proxy_model=False):
+        # TODO: We should probably also implement the second proxy version from the workshop paper
+        self._train_fraction_to_consider = train_fraction_to_consider
+        self._test_fraction_to_consider = test_fraction_to_consider
+        self._proxy_model = proxy_model
+        if proxy_model is True:
+            raise NotImplementedError("TODO")
+        self._shadow_pipeline_id = (train_fraction_to_consider, test_fraction_to_consider, proxy_model)
 
     @property
     def shadow_pipeline_id(self):
@@ -28,6 +34,18 @@ class LabelErrors(ShadowPipeline):
 
     def generate_shadow_pipeline_dag(self, dag: networkx.DiGraph) -> networkx.DiGraph:
         # pylint: disable=too-many-locals,too-many-statements
+        # 1. first search for predict labels
+
+        # 2. then get other info required for shapley:
+        # train_data_sample, train_label_sample, test_data_sample, test_label_sample,
+        # Also, have a configurable threshold how much of the train and test data gets used for the shapley stuff
+        #  because this is calculated using sampling, we also need to keep track of their indices (or at least prov ids)
+        # train_indices_to_consider
+        # output: the train indices to flip
+
+        # 3. then, look into label flipping:
+        # copy the original train labels, flip them. rerun the model fitting, run predict, and rerun the eval
+
         return dag
 
     def generate_final_report(self, extracted_plan_results: dict[str, any]) -> any:
