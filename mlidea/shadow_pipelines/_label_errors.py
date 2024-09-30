0689c5ac9cbff2d9b22caa2dfea20b56a830e065
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2024-09-30 11:55:54 +0200

    Add todos

--- mlidea/shadow_pipelines/_label_errors.py
+++ mlidea/shadow_pipelines/_label_errors.py
@@ -94,6 +94,8 @@ class LabelErrors(ShadowPipeline):
         extraction_node = get_intermediate_extraction_node(singleton, new_shapley_node, "label-errors-shapley-values")
         new_dag.add_edge(new_shapley_node, extraction_node, arg_index=0)
 
+        # TODO: Condition if shapley values are really negative and then the number of problematic rows is 0
+
         new_label_flip_node = DagNode(singleton.get_next_op_id(),
                                       BasicCodeLocation("Label Errors", None),
                                       OperatorContext(OperatorType.PROJECTION, None),
@@ -177,7 +179,7 @@ class LabelErrors(ShadowPipeline):
         extraction_node = get_intermediate_extraction_node(singleton, new_shapley_node, "label-errors-shapley-values")
         new_dag.add_edge(new_shapley_node, extraction_node, arg_index=0)
 
-        # TODO: Condition if shapley values are really negative
+        # TODO: Condition if shapley values are really negative and then the number of problematic rows is 0
 
         new_label_flip_indices_node = DagNode(singleton.get_next_op_id(),
                                               BasicCodeLocation("Label Errors", None),
@@ -206,6 +208,7 @@ class LabelErrors(ShadowPipeline):
         new_dag.add_edge(test_data_operators[0], new_label_flip_node, arg_index=4)
 
         # FIXME: Rerun predictions only on the diff like in data errors!!!
+        #  We now have the intermediate results
 
         new_predict_node = copy_node_with_new_id(singleton, predict_operators[0])
         new_dag.add_edge(new_label_flip_node, new_predict_node, arg_index=0)
