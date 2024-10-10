ca1b5d742708f1f32bc64aac347d49228ea82978
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2024-10-10 17:32:23 +0200

    Trying to fix some bugs that I cannot reproduce locally

--- example_pipelines/healthcare/healthcare.py
+++ example_pipelines/healthcare/healthcare.py
@@ -1,6 +1,8 @@
 """Predicting which patients are at a higher risk of complications"""
 import warnings
 import os
+
+import numpy
 import pandas as pd
 from fairlearn.metrics import equalized_odds_difference, MetricFrame, false_negative_rate
 from scikeras.wrappers import KerasClassifier
@@ -49,13 +51,15 @@ pipeline = Pipeline([
     ('learner', neural_net_with_grid_search)])
 
 train_data, test_data = train_test_split(data)
-test_data = test_data.reset_index(drop=True)
 model = pipeline.fit(train_data, train_data['label'])
 test_predictions = model.predict(test_data)
 print(f"Mean accuracy: {accuracy_score(test_data['label'], test_predictions)}")
 
+test_data = test_data.reset_index(drop=True)  # May help to prevent some low occurrence rate bugs in the following code
 sensitive_features = test_data[['race']]
 sensitive_features['race'] = sensitive_features['race'].astype(str)
+assert numpy.isnan(test_predictions).sum() == 0  # Trying to locate the bug that never occurs locally
+assert test_data['label'].isna().sum() == 0  # Trying to locate the bug that never occurs locally
 fnr_by_group = MetricFrame(metrics=false_negative_rate, y_pred=test_predictions, y_true=test_data['label'],
                            sensitive_features=sensitive_features)
 print(f"False-negative by group: {fnr_by_group.by_group}")
