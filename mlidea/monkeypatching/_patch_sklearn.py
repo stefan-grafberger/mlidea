2ea7cd50b5335983b38bbf158df12d5419c45dca
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2024-10-18 17:04:52 +0200

    WIP: Preparing reusing of cached results

--- mlidea/monkeypatching/_patch_sklearn.py
+++ mlidea/monkeypatching/_patch_sklearn.py
@@ -2403,6 +2403,7 @@ class SklearnLogisticRegressionPatching:
 
             # Estimator
             operator_context = OperatorContext(OperatorType.ESTIMATOR, function_info, self.mlinspect_non_data_func_args)
+            operator_call_info = OperatorCallInfo(operator_context, [train_data_node, train_labels_node])
             # input_dfs = [data_backend_result.annotated_dfobject, label_backend_result.annotated_dfobject]
             initial_func = partial(original, self, train_data_result, train_labels_result, *args[2:], **kwargs)
             optimizer_info, _ = capture_optimizer_info(singleton, operator_call_info, initial_func, self, estimator_transformer_state=self)
@@ -2465,23 +2466,28 @@ class SklearnLogisticRegressionPatching:
 
             original_predict = wrap_predict_func(gorilla.get_original_attribute(linear_model.LogisticRegression, 'predict'))
             initial_func_predict = partial(original_predict, self, test_data_result)
-            optimizer_info_predict, result_predict = capture_optimizer_info(singleton, operator_call_info, initial_func_predict)
             operator_context_predict = OperatorContext(OperatorType.PREDICT, function_info, {})
-            dag_node_predict = DagNode(singleton.get_next_op_id(operator_call_info),
+            estimator_dag_node = get_dag_node_for_id(self.mlinspect_estimator_node_id)
+            operator_call_info_predict = OperatorCallInfo(operator_context_predict, [estimator_dag_node, test_data_node])
+            optimizer_info_predict, result_predict = capture_optimizer_info(singleton, operator_call_info_predict,
+                                                                            initial_func_predict)
+            dag_node_predict = DagNode(singleton.get_next_op_id(operator_call_info_predict),
                                        BasicCodeLocation(caller_filename, lineno),
                                        operator_context_predict,
                                        DagNodeDetails("Logistic Regression", [], optimizer_info_predict),
                                        get_optional_code_info_or_none(optional_code_reference, optional_source_code),
                                        processing_func_predict)
-            estimator_dag_node = get_dag_node_for_id(self.mlinspect_estimator_node_id)
             function_call_result = FunctionCallResult(result_predict)
             add_dag_node(dag_node_predict, [estimator_dag_node, test_data_node], function_call_result)
 
             initial_func_score = partial(processing_func_score, result_predict, test_labels_result)
-            optimizer_info_score, result_score = capture_optimizer_info(singleton, operator_call_info, initial_func_score)
             non_data_kwargs = get_simple_non_data_kwargs(**kwargs)
             operator_context_score = OperatorContext(OperatorType.SCORE, function_info, non_data_kwargs)
-            dag_node_score = DagNode(singleton.get_next_op_id(operator_call_info),
+            operator_call_info_score = OperatorCallInfo(operator_context_predict,
+                                                          [dag_node_predict, test_labels_node])
+            optimizer_info_score, result_score = capture_optimizer_info(singleton, operator_call_info_score,
+                                                                        initial_func_score)
+            dag_node_score = DagNode(singleton.get_next_op_id(operator_call_info_score),
                                      BasicCodeLocation(caller_filename, lineno),
                                      operator_context_score,
                                      DagNodeDetails("Accuracy", [], optimizer_info_score),
@@ -2523,15 +2529,18 @@ class SklearnLogisticRegressionPatching:
 
             original_predict = wrap_predict_func(gorilla.get_original_attribute(linear_model.LogisticRegression, 'predict'))
             initial_func_predict = partial(original_predict, self, test_data_result)
-            optimizer_info_predict, result_predict = capture_optimizer_info(singleton, operator_call_info, initial_func_predict)
             operator_context_predict = OperatorContext(OperatorType.PREDICT, function_info, {})
-            dag_node_predict = DagNode(singleton.get_next_op_id(operator_call_info),
+            estimator_dag_node = get_dag_node_for_id(self.mlinspect_estimator_node_id)
+            operator_call_info_predict = OperatorCallInfo(operator_context_predict,
+                                                        [estimator_dag_node, test_data_node])
+            optimizer_info_predict, result_predict = capture_optimizer_info(singleton, operator_call_info_predict,
+                                                                            initial_func_predict)
+            dag_node_predict = DagNode(singleton.get_next_op_id(operator_call_info_predict),
                                        BasicCodeLocation(caller_filename, lineno),
                                        operator_context_predict,
                                        DagNodeDetails("Logistic Regression", [], optimizer_info_predict),
                                        get_optional_code_info_or_none(optional_code_reference, optional_source_code),
                                        processing_func_predict)
-            estimator_dag_node = get_dag_node_for_id(self.mlinspect_estimator_node_id)
             function_call_result = FunctionCallResult(result_predict)
             add_dag_node(dag_node_predict, [estimator_dag_node, test_data_node], function_call_result)
             new_result = function_call_result.function_result
